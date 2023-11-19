9624b51d5a8ccc842fbf8a876cf330b1c814a80c
Maicol Rua <maicolrua01@gmail.com>
2023-11-19 01:44:20 -0500

    Tercer commit - Se inicia ventana principal particionada

--- inicio/VentanaInicio.py
+++ inicio/VentanaInicio.py
@@ -1,12 +1,46 @@
 import sys
 
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QFormLayout, QHBoxLayout, QPushButton
 
 
 class VentanaInicio(QMainWindow):
 
     def __init__(self):
+
+        # Caracteristicas de la ventana principal
         super(VentanaInicio, self).__init__()
+        self.setWindowTitle("Animal city")
+        self.setWindowIcon(QtGui.QIcon('../imagenes/logo2.png'))
+        self.setFixedSize(950, 550)
+
+        self.fondo = QLabel(self)
+
+        self.horizontal = QHBoxLayout()
+
+        self.ladoIzquierdo = QFormLayout()
+
+        self.letrero1 = QLabel()
+        self.letrero1.setText("Bienvenidos")
+        self.letrero1.setStyleSheet("color: #000080;")
+        self.ladoIzquierdo.addRow(self.letrero1)
+        self.setLayout(self.horizontal)
+        self.horizontal.addLayout(self.ladoIzquierdo)
+        self.fondo.setLayout(self.horizontal)
+
+
+        #self.horizontal.addWidget(QPushButton("left-Most"))
+        #self.horizontal.addWidget(QPushButton("Center"), 1)
+        #self.horizontal.addWidget(QPushButton("Right-Most"), 2)
+        #self.lado1 = QFormLayout()
+        #self.letrero = QLabel()
+        #self.letrero.setText("Hola")
+        #self.letrero.setStyleSheet("color: #000080;")
+        #self.lado1.addRow(self.letrero)
+        #self.setLayout(self.horizontal)
+        #self.horizontal.addLayout(self.lado1)
+
+        #self.fondo.setLayout(self.horizontal)
 
 
 
