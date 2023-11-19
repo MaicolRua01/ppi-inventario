901b36a6dc4913db1936d61170ef5c3d0c109e67
Maicol Rua <maicolrua01@gmail.com>
2023-11-18 22:07:38 -0500

    Primer commit - Se agregan los archivos del proyecto

---
+++ inicio/VentanaInicio.py
@@ -0,0 +1,24 @@
+import sys
+
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+
+class VentanaInicio(QMainWindow):
+
+    def __init__(self):
+        super(VentanaInicio, self).__init__()
+
+
+
+if __name__ == '__main__':
+    # Hacer que la aplicacion se genere
+    app = QApplication(sys.argv)
+
+    # creamos un objeto tipo VentanaInicio con el nombre de Ventana Inicio
+    VentanaInicio = VentanaInicio()
+
+    # hacer que el objeto VentanaLogin se vea
+    VentanaInicio.show()
+
+    # para terminar la aplicacion
+    sys.exit(app.exec_())
